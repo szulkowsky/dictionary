f02e43b9450f5204a98462bd35e6320dee4c0e65
szulek <szulek0@gmail.com>
2018-06-13 20:10:27 +0200

    First Commit

---
+++ Dictionary/Dictionary/Dictionary.cpp
@@ -0,0 +1,12 @@
+// Dictionary.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello World" << std::endl;
+    return 0;
+}
+
